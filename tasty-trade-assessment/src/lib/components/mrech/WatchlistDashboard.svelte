241f6c2537167b588a84758b9842c8f322c13cd2
Michael Rechenberg <mrechenberg2@gmail.com>
2025-07-17 14:51:13 -0500

    Using Select from shadcn-svelte, I can switch between watchlists.

--- tasty-trade-assessment/src/lib/components/mrech/WatchlistDashboard.svelte
+++ tasty-trade-assessment/src/lib/components/mrech/WatchlistDashboard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { useQuery } from '@sveltestack/svelte-query';
+	import * as Select from '$lib/components/ui/select';
 
 	import type { GetAllUserWatchlistsOutput, Watchlist } from '$lib/tastytrade-api/watchlist';
 	import type { WatchlistDashboardProps } from './WatchlistDashboard.types';
@@ -21,10 +22,13 @@
 					return watchlistX['order-index'] - watchlistY['order-index'];
 				});
 
+				// Reset watchlists keyed by name
+				watchlistsKeyedByName = {};
 				allWatchlistData.output.forEach((watchlist) => {
 					watchlistsKeyedByName[watchlist.name] = watchlist;
 				});
 
+				// Set the selectd watchlist name to the first one in fetched set of watchlists (if a selection isn't present already)
 				selectedWatchlistName = selectedWatchlistName
 					? selectedWatchlistName
 					: allWatchlistData.output[0].name;
@@ -36,13 +40,21 @@
 <div>
 	{#if $allWatchlistsQuery.isLoading}
 		<div>Loading...</div>
-	{:else}
-		<div>The watchlists you have:</div>
-		<ul>
-			{#each $allWatchlistsQuery.data?.output ?? [] as watchlist (watchlist.name)}
-				<li>{watchlist.name}</li>
-			{/each}
-		</ul>
+	{:else if $allWatchlistsQuery.data}
+		<div>Selected watchlist:</div>
+		<Select.Root type="single" bind:value={selectedWatchlistName}>
+			<Select.Trigger>
+				{selectedWatchlistName ?? 'Choose a watchlist'}
+			</Select.Trigger>
+			<Select.Content>
+				{#each Object.keys(watchlistsKeyedByName).sort() as watchListName (watchListName)}
+					<Select.Item value={watchListName}>
+						{watchListName}
+					</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+
 		{#if selectedWatchlistName !== undefined}
 			<div>Rendering watchlist for selected watchlist [{selectedWatchlistName}]</div>
 			<WatchlistSymbols watchlist={watchlistsKeyedByName[selectedWatchlistName]} />
