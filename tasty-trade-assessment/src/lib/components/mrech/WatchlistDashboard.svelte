e15195ff3266facce05174862f6ade914da20f51
Michael Rechenberg <mrechenberg2@gmail.com>
2025-07-17 14:38:10 -0500

    Create a component WatchlistSymbols to render all the symbols of a given watchlist.  As of now, it is barebones (later I'll use data-table from shadcn-svelte for those)

--- tasty-trade-assessment/src/lib/components/mrech/WatchlistDashboard.svelte
+++ tasty-trade-assessment/src/lib/components/mrech/WatchlistDashboard.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
 	import { useQuery } from '@sveltestack/svelte-query';
 
-	import type { GetAllUserWatchlistsOutput } from '$lib/tastytrade-api/watchlist';
+	import type { GetAllUserWatchlistsOutput, Watchlist } from '$lib/tastytrade-api/watchlist';
 	import type { WatchlistDashboardProps } from './WatchlistDashboard.types';
+	import WatchlistSymbols from './WatchlistSymbols.svelte';
 
 	let { fetchAllWatchlists }: WatchlistDashboardProps = $props();
+
+	let selectedWatchlistName: string | undefined = $state(undefined);
+	let watchlistsKeyedByName: Record<string, Watchlist> = $state({});
 
 	const allWatchlistsQuery = useQuery<
 		GetAllUserWatchlistsOutput,
 		Error,
 		GetAllUserWatchlistsOutput
-	>('allWatchlists', fetchAllWatchlists);
+	>('allWatchlists', fetchAllWatchlists, {
+		onSuccess: (allWatchlistData) => {
+			if (allWatchlistData.output && allWatchlistData.output.length > 0) {
+				allWatchlistData.output.sort((watchlistX, watchlistY) => {
+					return watchlistX['order-index'] - watchlistY['order-index'];
+				});
+
+				allWatchlistData.output.forEach((watchlist) => {
+					watchlistsKeyedByName[watchlist.name] = watchlist;
+				});
+
+				selectedWatchlistName = selectedWatchlistName
+					? selectedWatchlistName
+					: allWatchlistData.output[0].name;
+			}
+		}
+	});
 </script>
 
 <div>
@@ -23,5 +43,9 @@
 				<li>{watchlist.name}</li>
 			{/each}
 		</ul>
+		{#if selectedWatchlistName !== undefined}
+			<div>Rendering watchlist for selected watchlist [{selectedWatchlistName}]</div>
+			<WatchlistSymbols watchlist={watchlistsKeyedByName[selectedWatchlistName]} />
+		{/if}
 	{/if}
 </div>
